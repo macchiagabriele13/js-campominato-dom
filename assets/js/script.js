ea56fd14e5cfff18b77a4e14140a6bd0a7657d81
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-18 15:12:21 +0200

    commit - confronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,8 @@ console.log(containerGrid);
 } */
 let numbMax = 100
 
+let bombMax = 16
+
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 /* Devo rendere flessibile la griglia attraverso una funzione */
@@ -30,12 +32,13 @@ const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)
 bottoneGeneratore.addEventListener("click", function () {
     console.log("Bottone cliccato");
 
-   
+
     boxGenerator(numbMax, containerGrid)
 
     function boxGenerator(numero, griglia) {
         for (let i = 1; i <= numero; i++) {
             griglia.innerHTML += `<div class="box">${i}</div>`
+
         }
 
         let boxEl = document.querySelectorAll('.box')
@@ -46,39 +49,61 @@ bottoneGeneratore.addEventListener("click", function () {
             console.log(currentBox);
 
             currentBox.addEventListener("click", function () {
-                currentBox.classList.toggle("azzurro")
-                console.log(currentBox.innerHTML)
+
+                if(bombs.includes(currentBox) ){
+                    currentBox.classList.toggle("rosso")
+                   
+                } else if(!bombs.includes(currentBox) ) {
+                    currentBox.classList.toggle("azzurro")
+                    
+                }
+
+                
             })
+
         }
+        const bombs = generateBombs(1, numbMax)
+        console.log(bombs);
+
+      
+    
+
 
 
     }
 })
 
 
-   /* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. */
-    /* Attenzione:
-    **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali. */
+/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. */
+/* Attenzione:
+**nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali. */
 
+/* const bombs = generateBombs(1, numbMax)
+console.log(bombs); */
 
-    function generateBombs(bombe) {
-        const bombs = []
-        while (bombs.length !== 16) {
-            const bomb = getRandomNumber(1, numbMax);
-            if (!bombs.includes(bomb)) {
-                bombs.push(bomb);
-            };
+function generateBombs(min, max) {
+    const bombs = []
+    while (bombs.length !== bombMax) {
+        const bomb = getRandomNumber(min, max)
+
+        if (!bombs.includes(bomb)) {
+            bombs.push(bomb)
+
         }
-        return bombs;
-    }
-    
-  }
 
-/* In seguito l'utente clicca su una cella: */ 
+    }
+    return bombs
+}
+
+/* In seguito l'utente clicca su una cella: */
 
 /* se il numero è presente nella lista dei numeri generati  */
 
-/* abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. */ 
+/* abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. */
 
-/* Altrimenti la cella cliccata si colora di azzurro */ 
+/* Altrimenti la cella cliccata si colora di azzurro */
 /* e l'utente può continuare a cliccare sulle altre celle. */
+
+
+/* 
+ok, una volta che hai le bombe, usale nel punto in cui fai il ciclo per generare le celle. Li hai giá accesso alla singola cella e quindi puoi usare innerText per prendere il suo numero e confrontarlo con la lista di bombe per vedere se é incluso. Se é incluso allora hai cliccato su una bomba e quindi al click colori la cella di rosso */
